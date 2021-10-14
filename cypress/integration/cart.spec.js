381a9e407f1c31b27d23349f101b6b79dcb4359d
Lukas Prochazka <prochazka.lu@gmail.com>
2021-10-14 16:45:32 +0200

    test updates

--- cypress/integration/cart.spec.js
+++ cypress/integration/cart.spec.js
@@ -3,41 +3,44 @@ describe('add and remove quantity from cart and delete the cart', () => {
     cy.visit('http://localhost:3000');
     // get to products page
     cy.wait(1000);
-    cy.get('[data-cy="header-allProducts-link"]').should('exist').click();
+    cy.get('[data-cy="header-allProducts-link"]').should('be.visible').click();
     // get to the first single product item
     cy.wait(1000);
-    cy.contains('Nigeria Away Jersey').should('exist');
-    cy.contains('Colombia Home Jersey').should('exist').click();
+    cy.contains('Nigeria Away Jersey').should('be.visible');
+    cy.contains('Colombia Home Jersey').should('be.visible').click();
     // add to Cart
     cy.wait(1000);
-    cy.contains('ADD TO CART').should('exist');
+    cy.contains('ADD TO CART').should('be.visible');
     // put first item in cart
-    cy.get('[data-cy="add-to-Cart"]').should('exist').click();
+    cy.get('[data-cy="add-to-Cart"]').should('be.visible').click();
     // go back to all products
     cy.wait(1000);
-    cy.get('[data-cy="header-allProducts-link"]').should('exist').click();
+    cy.get('[data-cy="header-allProducts-link"]').should('be.visible').click();
     // get to the last single product item
     cy.wait(1000);
-    cy.contains('England Away Jersey').should('exist');
-    cy.contains('Nigeria Away Jersey').should('exist').click();
+    cy.contains('England Away Jersey').should('be.visible');
+    cy.contains('Nigeria Away Jersey').should('be.visible').click();
     // add to Cart
     cy.wait(1000);
-    cy.contains('ADD TO CART').should('exist');
+    cy.contains('ADD TO CART').should('be.visible');
     // put last item in cart
-    cy.get('[data-cy="add-to-Cart"]').should('exist').click();
+    cy.get('[data-cy="add-to-Cart"]').should('be.visible').click();
     // press + 2 times first item
     cy.get('[data-cy="plus-quantity-in-cart"')
-      .should('exist')
+      .should('be.visible')
       .first()
       .dblclick();
     // press - 1 time first item
-    cy.get('[data-cy="minus-quantity-in-cart"').should('exist').first().click();
+    cy.get('[data-cy="minus-quantity-in-cart"')
+      .should('be.visible')
+      .first()
+      .click();
     // press - 2 times last item, so it delete from cart
     cy.get('[data-cy="minus-quantity-in-cart"')
-      .should('exist')
+      .should('be.visible')
       .last()
       .dblclick();
     // press delete all from cart
-    cy.get('[data-cy="deleteAll-in-Cart"').should('exist').click();
+    cy.get('[data-cy="deleteAll-in-Cart"').should('be.visible').click();
   });
 });
