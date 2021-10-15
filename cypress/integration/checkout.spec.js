ac69ae59992cb9f28def05bab092bbb88fa6d476
Lukas Prochazka <prochazka.lu@gmail.com>
2021-10-15 16:14:14 +0200

    changed test

--- cypress/integration/checkout.spec.js
+++ cypress/integration/checkout.spec.js
@@ -11,8 +11,8 @@ describe('add 1 item to cart and checkout to thank you page', () => {
     // checkout page
     cy.wait(2000).contains('Colombia Home Jersey').should('be.visible');
     cy.wait(2000).get('[data-cy="move-to-Checkout"]').click();
-    // press pay
-    cy.wait(2000).get('[data-cy="payment"]').should('be.visible').click();
+    /*    // press pay
+    cy.wait(2000).get('[data-cy="payment"]').should('be.visible').click(); */
     cy.contains('please enter your first name!').should('be.visible');
     // enter all forms from checkout page
     cy.wait(2000).contains('First Name').should('be.visible');
