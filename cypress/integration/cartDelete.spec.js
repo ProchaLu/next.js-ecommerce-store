0c3cdf37a8178ddd7ce29c6d7dbb3850ef9aea88
Lukas Prochazka <prochazka.lu@gmail.com>
2021-10-15 15:49:57 +0200

    changed tests

--- cypress/integration/cartDelete.spec.js
+++ cypress/integration/cartDelete.spec.js
@@ -1,11 +1,6 @@
 describe('add and remove quantity from cart and delete the cart', () => {
-  it('visits singleProduct page, clicks item, clicks + / - and delete from cart', () => {
-    cy.visit('http://localhost:3000');
-    // get to products page
-    cy.wait(2000)
-      .get('[data-cy="header-allProducts-link"]')
-      .should('be.visible')
-      .click();
+  it('visits singleProduct page, clicks item and delete from cart', () => {
+    cy.visit('http://localhost:3000/products');
     // get to the first single product item
     cy.wait(2000);
     cy.contains('Nigeria Away Jersey').should('be.visible');
@@ -29,21 +24,8 @@ describe('add and remove quantity from cart and delete the cart', () => {
     cy.get('[data-cy="add-to-Cart"]').should('be.visible').click();
     // press + 2 times first item
     cy.wait(2000)
-      .get('[data-cy="plus-quantity-in-cart"')
+      .get('[data-cy="deleteAll-in-Cart"')
       .should('be.visible')
-      .first()
-      .dblclick();
-    // press - 1 time first item
-    cy.get('[data-cy="minus-quantity-in-cart"')
-      .should('be.visible')
-      .first()
       .click();
-    // press - 2 times last item, so it delete from cart
-    cy.get('[data-cy="minus-quantity-in-cart"')
-      .should('be.visible')
-      .last()
-      .dblclick();
-    // press delete all from cart
-    cy.get('[data-cy="deleteAll-in-Cart"').should('be.visible').click();
   });
 });
