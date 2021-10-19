041f8d92ca847692f4ec667babee36ae1f150ea8
Lukas Prochazka <prochazka.lu@gmail.com>
2021-10-19 17:08:47 +0200

    changed feature site

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -1,10 +1,11 @@
 import { css } from '@emotion/react';
 import Image from 'next/image';
-import new4 from '../public/images/Belgium.jpeg';
+
+/* import new4 from '../public/images/Belgium.jpeg';
 import new3 from '../public/images/Brasil.jpeg';
 import new1 from '../public/images/Colombia.jpeg';
 import new5 from '../public/images/Germany.jpeg';
-import new2 from '../public/images/Nigeria.jpeg';
+import new2 from '../public/images/Nigeria.jpeg'; */
 
 export default function Feature() {
   const featureWrapper = css`
@@ -28,11 +29,11 @@ export default function Feature() {
       <h1>OUR NEW ARRIVALS</h1>
       <p>Check out our new arrivals down below</p>
       <div css={featureContent}>
-        <Image src={new1} alt="new arrival " />
+        {/*         <Image src={new1} alt="new arrival " />
         <Image src={new2} alt="new arrival " />
         <Image src={new3} alt="new arrival " />
         <Image src={new4} alt="new arrival " />
-        <Image src={new5} alt="new arrival " />
+        <Image src={new5} alt="new arrival " /> */}
       </div>
     </div>
   );
